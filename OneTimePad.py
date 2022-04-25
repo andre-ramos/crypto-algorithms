f5163d1aab050cfbe4f74b77be1dfac36a334fd9
andre-ramos <andre.rammos@outlook.com>
2022-04-25 10:56:03 -0300

    added attacker point of view

--- OneTimePad.py
+++ OneTimePad.py
@@ -8,11 +8,18 @@ def xor_bytes(key_stream, message):
     length = min(len(key_stream), len(message))
     return bytes([key_stream[i] ^ message[i] for i in range(length)])
 
-message = "HELLO FROM ME"
-# convert string to binary
+
+message = "DO ATTACK"
+# convert string(Unicode) to bytes (utf-8)
 message = message.encode()
 key_stream = generate_key_stream(len(message))
 cipher = xor_bytes(key_stream, message)
 print(key_stream)
+
+
 print(cipher)
-print(xor_bytes(key_stream,cipher))
+message = "NO ATTACK"
+message = message.encode()
+guess_key_stream = xor_bytes(message, cipher)
+print(xor_bytes(guess_key_stream, cipher))
+#print(xor_bytes(key_stream,cipher))
