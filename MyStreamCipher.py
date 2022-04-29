fb287a16d2174c068af7ee8be933c51c3ae6bdea
andre-ramos <andre.rammos@outlook.com>
2022-04-29 18:33:58 -0300

    modification example

--- MyStreamCipher.py
+++ MyStreamCipher.py
@@ -37,12 +37,23 @@ def transmit_(cipher):
         a = a + 1
     return bytes(b)
 
+# if somebody modify the message
+def modification(cipher):
+    mod = [0]*len(cipher)
+    mod[10] = ord(' ') ^ ord('1')
+    mod[11] = ord(' ') ^ ord('0')
+    mod[12] = ord('0') ^ ord('1')
+    return bytes([mod[i] ^ cipher[i] for i in range(len(cipher))])
+
+
 key = KeyStream(10)
-message = "This is my message".encode()
+message = "Send Bob:   10$".encode()
 cipher = encrypt(key, message)
 print(cipher)
 
-cipher = transmit_(cipher)
+# cipher = transmit_(cipher)
+# This is Bob modifying the message
+cipher = modification(cipher)
 
 key = KeyStream(10)
 message = encrypt(key, cipher)
