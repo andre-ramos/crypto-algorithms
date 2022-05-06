be77d6cc3c4baf9c675d962bffe5ef06dccec358
andre-ramos <andre.rammos@outlook.com>
2022-05-06 11:09:25 -0300

    modifying des cipher

--- SimpleDESExample.py
+++ SimpleDESExample.py
@@ -1,7 +1,21 @@
 from pyDes import *
 
 
-message = "0123456701234567"
+def modify_many_bits(cipher):
+    mod = [0]*len(cipher)
+    print("Mod: "+str(mod))
+    mod[10] = ord(' ') ^ ord('1')
+    mod[11] = ord(' ') ^ ord('0')
+    mod[12] = ord('1') ^ ord('0')
+    return bytes([mod[i] ^ cipher[i] for i in range(len(cipher))])
+
+def modify(cipher):
+    mod = [0]*len(cipher)
+    print("Mod: "+str(mod))
+    mod[8] = 1
+    return bytes([mod[i] ^ cipher[i] for i in range(len(cipher))])
+
+message = "Give Bob       10$"
 key = "DESCRYPT"
 iv = bytes([0]*8)
 # create the cipher here
@@ -12,10 +26,12 @@ cipher = k.encrypt(message)
 # encrypt the message to cipher
 print("Length of plain text:", len(message))
 print("Length of cipher text:", len(cipher))
-print("Encrypted:", cipher[0:8])
-print("Encrypted:", cipher[8:16])
-print("Encrypted:", cipher[16:])
+print("Encrypted:", cipher)
+
+
 # Bob decrypting the cipher text
+cipher = modify(cipher)
+
 # decrypt the cipher to message
 message = k.decrypt(cipher)
 print("Decrypted:", message)
